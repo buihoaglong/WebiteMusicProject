51977f609d954cfc1fe629d26985b093e48db60b
buihoaglong <MontestBHL@gmail.com>
2025-03-24 09:53:37 +0700

    upd
    upd Frontend

---
+++ WebsiteMusicFrontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import SideBar from './components/SideBar'
+import Player from './components/Player'
+
+const App = () => {
+  return (
+    <div className='h-screen bg-black'>
+      <div className='h-[90%] flex'>
+        <SideBar/>
+      </div>
+      <Player/>
+    </div>
+  )
+}
+
+export default App
