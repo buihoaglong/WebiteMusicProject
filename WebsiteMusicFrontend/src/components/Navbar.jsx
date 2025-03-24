8690bcae1ea8c952763f6baae92872f75db4b90d
buihoaglong <MontestBHL@gmail.com>
2025-03-24 10:55:44 +0700

    Upd
    --Update
    + DisplayHome
    +SongItem ( hien thi)
    + AlbumItem ( hien thi) - Viet tiep code o day
    + Navbar

--- WebsiteMusicFrontend/src/components/Navbar.jsx
+++ WebsiteMusicFrontend/src/components/Navbar.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../assets/assets";
 
 const Navbar = () => {
   return (
-    <div >
-      
-    </div>
-  )
-}
+    <>
+      <div className="w-full flex justify-between items-center font-semibold">
+        <div className="flex items-center gap-2">
+          <img
+            className="w-8 bg-black p-2 rounded-2x1 cursor-pointer"
+            src={assets.arrow_left}
+            alt=""
+          />
+          <img
+            className="w-8 bg-black p-2 rounded-2x1 cursor-pointer"
+            src={assets.arrow_right}
+            alt=""
+          />
+        </div>
 
-export default Navbar
+        <div className="flex items-start gap-4">
+          <p className="bg-white text-black text-[15px] px-4 py-1 rounded-full hidden md:block cursor-pointer">
+            Explore Premium
+          </p>
+          <p className="bg-black py-1 px-3 rounded-full text-[15px] cursor-pointer">
+            Install App
+          </p>
+          <p className="bg-purple-500 text-black w-7 h-7 rounded-full flex flex-items items-center justify-center ">
+            D
+          </p>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-2 mt-4">
+        <p className="bg-white text-black px-4 py-1 rounded-full  cursor-pointer">
+          All
+        </p>
+        <p className="bg-black text-white px-4 py-1 rounded-full  cursor-pointer">
+          Music
+        </p>
+        <p className="bg-black text-white px-4 py-1 rounded-full  cursor-pointer">
+          Podcasts
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
